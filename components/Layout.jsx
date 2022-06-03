19646a732f6b1a046c8ccbe3a4b78c5a1babe758
aashankhan2981 <aashankhan2981@gmail.com>
2022-06-04 00:45:00 +0500

    3 pages

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,6 +4,7 @@ import Header from './Header'
 import { useRouter } from 'next/router'
 import Sidebar from './Sidebar'
 import Head from 'next/head'
+import Script from 'next/script'
 // import 'tw-elements';
 const Layout = ({children}) => {
   const withoutSiderBar = ['/','/login','/register']
@@ -14,8 +15,12 @@ const Layout = ({children}) => {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" />
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/index.min.css" />
-<script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"></script>
     </Head>
+    <Script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js" />
+   
+
+  
+
     <div className='bg-site_black flex flex-col items-center'>
         {withoutSiderBar.includes(router.pathname) ?
        <> <Header/>
